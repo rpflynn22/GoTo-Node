c01eb1ab56ad61660a344cb733e23f09fdff9d98
Alex McKinney <alexander.r.mckinney@gmail.com>
2014-07-19 20:05:15 -0700

    Zach's Notes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var util = require('./util.js');
+var mongoose = require('mongoose');
 var router = express.Router();
+
+var ObjectId = mongoose.Types.ObjectId;
+var errorCallback = util.errorCallback;
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -7,3 +12,19 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+
+
+/** Controller Methods */
+exports.getFriends = function(req, res) {
+  model.user.getFriends(req.body.username, function(err, data) {
+    if(err) { res.end(JSON.stringify(err)) };
+    res.end(JSON.stringify(data));
+  });
+}
+
+exports.inviteFriend = function(req, res) {
+  model.user.inviteFriend(req.body.username, req.body.friend, function(err, data) {
+    if(err) { res.end(JSON.stringify(err)) };
+    res.end(JSON.stringify(data));
+  });
+}
